2e6654402f3a8c03e6cafb3faae898abf51f2816
tanverified <khantanveer2u@gmail.com>
2021-07-26 16:35:38 +0530

    project start

--- navbar/src/Components/Services.js
+++ navbar/src/Components/Services.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React from "react";
+import { useParams, useLocation } from "react-router-dom";
 
 const Services = () => {
-    return ( 
-        <h1>This is services page</h1>
-     );
-}
- 
+  const { web } = useParams();
+  const location = useLocation();
+//   console.log(location);
+  return (
+    <>
+      <h1>This is services page</h1>
+      <h2>This is {web}</h2>
+      <h3>My current location is <span>{location.pathname }</span></h3>
+      {location.pathname === `/services/web` ? (
+      <button onClick={() => alert('You are on web services')}>Web Services</button>
+      ):null}
+    </>
+  );
+
+};
+
 export default Services;
