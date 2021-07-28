33be218fff6cc36e4323dfb10b36bdb10d5c5935
tanverified <khantanveer2u@gmail.com>
2021-07-28 22:23:26 +0530

    birthdatReminder finished

--- birthday_reminder/src/App.jsx
+++ birthday_reminder/src/App.jsx
@@ -1,20 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "./Card";
 import data from "./data";
 
+
 const App = () => {
+
+  const [peoples, setPeoples] = useState(data);
+
+
   return (
     <main className="main">
       <section className="container">
         <h3>0 Peoples Birthday</h3>
-
-     {
-      data.map((e) => {
-        return(
-          <Card key={e.id} name={e.name} img={e.image} age={e.age} />
-          )
-      })
-     }
+        {peoples.map((person) => {
+          return (
+            <Card
+              key={person.id}
+              name={person.name}
+              img={person.image}
+              age={person.age}
+            />
+          );
+        })}
+        <button onClick={()=>{setPeoples([])}}>Clear all</button>
       </section>
     </main>
   );
---
+++ birthday_reminder/src/Peoples.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Peoples(props) {
+  return (
+    <>
+      
+    </>
+  );
+}
+
+export default Peoples;
